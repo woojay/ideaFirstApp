dcb391d7adf81bf17fb56f10d05f0bd4c5e88459
woojay <fullyanalog-communicator@yahoo.com>
2016-02-15 14:05:48 -0800

    Initial commit

---
+++ src/com/example/ideaFirstApp/ideaFirstActivity.java
@@ -0,0 +1,15 @@
+package com.example.ideaFirstApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ideaFirstActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
